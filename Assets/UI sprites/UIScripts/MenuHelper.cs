a87b7a3cbb59419ad70673e078f154cdd09fc43b
Drina <Drinadomandiola@gmail.com>
2020-06-04 12:31:08 +1200

    Menu UI almost Done

---
+++ Assets/UI sprites/UIScripts/MenuHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHelper : MonoBehaviour
+{
+    public TriggerExit Exit;
+
+
+
+   public void Canvas1()
+    {
+        Exit.menuTrigger1();
+       
+    }
+
+    public void Canvas2()
+    {
+        Exit.menuTrigger2();
+    }
+
+    
+    
+}
